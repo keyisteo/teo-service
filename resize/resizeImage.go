97be1093a758927964ebdc70b5aa3fc00e2300ee
keyisteo <keyisteo.kingdom@gmail.com>
2017-11-13 13:21:43 +0700

    include resizer from nfnt github

---
+++ resize/resizeImage.go
@@ -0,0 +1,36 @@
+package resize
+
+import (
+	"image/jpeg"
+	"log"
+	"os"
+	"teo-service/resize"
+)
+
+func resizeImage(fileName string, fileRename string) {
+	// open "test.jpg"
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// decode jpeg into image.Image
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
+
+	// resize to width 1000 using Lanczos resampling
+	// and preserve aspect ratio
+	m := resize.Resize(1000, 0, img, resize.Lanczos3)
+
+	out, err := os.Create(fileRename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	// write new image to file
+	jpeg.Encode(out, m, nil)
+}

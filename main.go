37f90f670202a9b1537183803831ff6e00143d65
keyisteo <keyisteo.kingdom@gmail.com>
2017-12-12 15:40:55 +0700

    add ORM

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	_ "teo-service/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+	_ "teo-service/routers"
+	"time"
 )
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+
+	orm.RegisterDataBase("default", "mysql", "root:root@/kesat?charset=utf8")
+
+	orm.DefaultTimeLoc = time.UTC
+}
 
 func main() {
 	beego.Run()
 }
-

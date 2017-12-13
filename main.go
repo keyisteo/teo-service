ccda4474f80c10004e83e0d3be5711bacdac9f2a
keyisteo <keyisteo.kingdom@gmail.com>
2017-12-13 13:03:29 +0700

    Session configuration

--- main.go
+++ main.go
@@ -3,19 +3,29 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/session"
 	_ "github.com/go-sql-driver/mysql"
 	_ "teo-service/routers"
 	"time"
 )
 
 func init() {
+
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 
 	orm.RegisterDataBase("default", "mysql", "root:root@/kesat?charset=utf8")
 
 	orm.DefaultTimeLoc = time.UTC
+
 }
 
 func main() {
+	sessionconf := &session.ManagerConfig{
+		CookieName: "begoosessionID",
+		Gclifetime: 3600,
+	}
+	beego.GlobalSessions, _ = session.NewManager("memory", sessionconf)
+	go beego.GlobalSessions.GC()
+
 	beego.Run()
 }
